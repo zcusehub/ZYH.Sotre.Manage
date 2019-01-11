908ff4cb307dd8cb85443ca3a5542570ea9d37cb
张聪 <451113225@qq.com>
2019-01-11 17:27:16 +0800

    110

--- code/ZYH.Store.Manage.API/ZYH.Store.Manage.WebAPI/Controllers/ValuesController.cs
+++ code/ZYH.Store.Manage.API/ZYH.Store.Manage.WebAPI/Controllers/ValuesController.cs
@@ -2,23 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using ZYH.Common;
 
 namespace ZYH.Store.Manage.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Microsoft.AspNetCore.Authorization.Authorize]
     public class ValuesController : ControllerBase
     {
         // GET api/values
         [HttpGet]
+        [AllowAnonymous]
         public ActionResult<IEnumerable<string>> Get()
         {
+            IConfigurationBuilder builder=new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json");
+            var root=builder.Build();
+            var kk = root["DB:SqlServer"];
+            var k1=ConfigAppSettingsJson.Instance().GetValueByKey("DB:SqlServer");
+            var k2=ConfigAppSettingsJson.Instance().GetValueByKey("DB:SqlServer1");
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
+        [HttpGet("{id}")] 
         public ActionResult<string> Get(int id)
         {
             string heads = string.Join(',', HttpContext.Request.Headers.Keys.Select(p => p.ToString()).ToList());
